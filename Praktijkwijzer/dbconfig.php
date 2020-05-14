0d4e4e5cbba5cf20a27a324638cb41be3dce2321
mwoaksx <0176444@student.roc-nijmegen>
2020-05-14 10:53:12 +0200

    Setup

---
+++ Praktijkwijzer/dbconfig.php
@@ -0,0 +1,17 @@
+<?php
+define('MYSQL_USER','root');
+define('MYSQL_PASSWORD','');
+define('MYSQL_HOST','localhost');
+define('MYSQL_DATABASE','campingmaasvalei');
+
+try 
+{
+    $conn = new PDO("mysql:host=".MYSQL_HOST.";dbname=".MYSQL_DATABASE, MYSQL_USER, MYSQL_PASSWORD);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "<script>alert('Database connected');</script>";
+}
+catch(PDOException $e) 
+{
+    echo "Connection failed: " . $e->getMessage();
+}
+?>
